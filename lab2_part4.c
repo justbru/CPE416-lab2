3ac78e5461baf44c29fdacfb4e988d1e34a31d12
justbru <j.brunings2020@gmail.com>
2023-10-17 09:06:37 -0700

    completed part 4

--- lab2_part4.c
+++ lab2_part4.c
@@ -3,6 +3,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <string.h>
+#include <stdlib.h>
 
 void motor(uint8_t num, int8_t speed)
 {
@@ -11,7 +12,7 @@ void motor(uint8_t num, int8_t speed)
 
 int calculateErr(u08 actual)
 {
-    u08 error = 220 - actual;
+    u08 error = 100 - actual;
     if (error > 100)
         return 100;
     if (error < 0)
@@ -27,34 +28,88 @@ int main(void)
     u08 black = 220;
     u08 diff = black - white;
 
+    u16 timeSinceTurning = 0;
+
     init();
     set_servo(0, 127);
     set_servo(1, 127);
     digital_dir(5, 0);
     digital_dir(4, 0);
 
+    motor(0, 0);
+    motor(1, 0);
+
     while (1)
     {
-        rightSensor = analog(5);
+        timeSinceTurning += 20;
+
         leftSensor = analog(4);
+        rightSensor = analog(5) - 5;
+
+        u08 maxSpeed = 20;
+        clear_screen();
+        print_num(leftSensor);
+        print_string(" ");
+        print_num(rightSensor);
+        //_delay_ms(20);
+
+        // Speed up if less than 0 (should balance out the robot in the center of the line)
+        if ((leftSensor < 215))
+        {
+            motor(0, maxSpeed + ((210 - leftSensor) * 5));
+        }
+        if ((rightSensor < 215))
+        {
+            motor(1, (maxSpeed + ((210 - rightSensor) * 5)) * -1);
+        }
+
+        if ((leftSensor > 210 && rightSensor > 210) || abs(leftSensor - rightSensor) < 2)
+        {
+            motor(0, 40);
+            motor(1, -40);
+            _delay_ms(100);
+        }
+        else if (rightSensor > 214)
+        { // turn right
+            if (timeSinceTurning > 1000)
+            {
+                motor(1, 0);
+                motor(0, 39);
+                _delay_ms(1950);
+                timeSinceTurning = 0;
+            }
+        }
+        else if (leftSensor > 214)
+        {
+            if (timeSinceTurning > 1000)
+            {
+                motor(1, -39);
+                motor(0, 0);
+                _delay_ms(1950);
+                timeSinceTurning = 0;
+            }
+        }
+
+        _delay_ms(1);
 
         // all cases
-        if (leftSensor < 210 && rightSensor > 215) // right turn (box)
-        {
-            motor(1, 100);
-            motor(0, 100);
-        }
-        else if (rightSensor < 200 && leftSensor > 215) // right turn
-        {
-            motor(1, (100 - (calculateErr(rightSensor))) * 0.3);
-            motor(0, -100);
-        }
-        else // standard case of following the line
-        {
-            motor(0, (100 - (calculateErr(rightSensor))) * 0.3);
-            motor(1, (100 - (calculateErr(leftSensor))) * -0.3);
-        }
+        // if (leftSensor < 200 && rightSensor > 205) // right turn (box)
+        // {
+        //     motor(1, 100);
+        //     motor(0, 100);
+        //     //_delay_ms(200);
+        // }
+        // else if (rightSensor < 210 && leftSensor > 215) // left turn
+        // {
+        //     motor(1, -100);
+        //     motor(0, -100);
+        // }
+        // else // standard case of following the line
+        // {
+        // motor(0, (100 - (calculateErr(rightSensor))) * 0.3);
+        // motor(1, (100 - (calculateErr(leftSensor))) * -0.3);
+        //}
     }
-    
+
     return 0;
 }
